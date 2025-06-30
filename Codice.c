ddd2bc1cef392452aef9cca968ba9c9bef7ee1e4
Filippo Depetri <filippo.depetri@mail.polimi.it>
2025-06-30 18:11:57 +0200

    Fix ROTTA_AEREA definition to use single quotes for character constant

--- Codice.c
+++ Codice.c
@@ -29,7 +29,7 @@ Consegna:   Movhex è una compagnia di autotrasporti che dispone di una flotta d
 #define DEBUG
 #define INIT 'i'
 #define CAMBIO_COSTO 'c'
-#define ROTTA_AEREA "o"
+#define ROTTA_AEREA 'o'
 #define COSTO_VIAGGIO 't'
 #define AFFERMATIVO "OK"
 #define FALSO "KO"
