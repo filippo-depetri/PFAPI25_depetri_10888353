111b3294908d189e9ff526da85eb2a3207a23ddc
Filippo Depetri <filippo.depetri@mail.polimi.it>
2025-07-11 18:33:55 +0200

    fix

--- nuovo.c
+++ nuovo.c
@@ -577,7 +577,8 @@ void swap(heap *nodo1, heap *nodo2){
     *nodo2=temp;
 }
 void push_heap(heap *nodo, int *size, int x, int y, int costo){
-    int pos=*size++;
+    *size++;
+    int pos=*size;
     nodo[pos].costo_heap=costo;
     nodo[pos].x_heap=x;
     nodo[pos].y_heap=y;
