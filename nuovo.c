a9b7f4078773cf946dcdc51477322649b9e0ff6b
Filippo Depetri <filippo.depetri@mail.polimi.it>
2025-07-23 18:15:15 +0200

    fix

--- nuovo.c
+++ nuovo.c
@@ -426,6 +426,7 @@ void comando_travel_cost(map *mappa, int x1, int y1, int x2, int y2, FILE *outpu
     if (mappa->esagoni==NULL)
     {
         fprintf(output, "%d\n", NOT_VALID_TRAVEL);
+        return;
     }
     if (x1>=dim_mappa.dimx || x1<0 || x2>=dim_mappa.dimx || x2<0 || y1>=dim_mappa.dimy || y1<0 || y2>=dim_mappa.dimy || y2<0 || mappa->esagoni[x1*dim_mappa.dimy+y1][0]==0)   //check se sono nei limiti mappa
     {
@@ -477,7 +478,7 @@ void comando_travel_cost(map *mappa, int x1, int y1, int x2, int y2, FILE *outpu
                                 push_heap(nodi, &dim_heap, mappa->rotte_aeree[i][2], mappa->rotte_aeree[i][3], costi[successivo]);
                             }
                         }
-                        if (costi[successivo]<MAX_COST)
+                        else
                         {
                             if (visitati[successivo]==0 && costi[index] + costo_corrente<costi[successivo])
                             {
@@ -508,7 +509,7 @@ void comando_travel_cost(map *mappa, int x1, int y1, int x2, int y2, FILE *outpu
                             push_heap(nodi, &dim_heap, coordinate[i][0], coordinate[i][1], costi[successivo]);
                         }
                     }
-                    if (costi[successivo]<MAX_COST)
+                    else
                     {
                         if (visitati[successivo]==0 && costi[index] + costo_corrente<costi[successivo])
                         {
